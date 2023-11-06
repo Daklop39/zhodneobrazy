2fb99d96192d648ef3a480b9d205d7099163d5b5
Daklop39 <danko.reya@gmail.com>
2023-11-06 16:45:04 +0100

    Update main.py

--- main.py
+++ main.py
@@ -12,11 +12,7 @@ turtle.hideturtle()
 stx=random.randint(10,100)
 sty=random.randint(10,100)
 
-stxs=0
-stys=0
-turtle.pensize(5)
-turtle.fd(0)
-turtle.pensize(1)
+
 
 type=input("What type of")    
 
@@ -24,15 +20,41 @@ N=int(input("Diameter of oval"))
 N=N/2
 def type1():                                            #stredova sumernost
     canvas.create_oval(stx-N,sty-N,stx+N,sty+N)
-    turtle.setposition(100, -110)
+    turtle.penup
+    turtle.setposition(stx, sty)
+    turtle.pendown
+    stxs=0
+    stys=0
+    turtle.pensize(5)
+    turtle.fd(0)
+    turtle.pensize(1)
 
-if type=="1":
+def type2():                                            #osova sumernost
+    canvas.create_oval(stx-N,sty-N,stx+N,sty+N)
+    canvas.create_line(0,0,0,100)
+
+def type3():                                            #posunutie
+    canvas.create_oval(stx-N,sty-N,stx+N,sty+N)
+
+
+
+
+def type4():                                            #rotacia
+    canvas.create_oval(stx-N,sty-N,stx+N,sty+N)
+
+
+
+
+
+if type == "1":
     type1()
 
-
-
-
-
+if type == "2":
+    type2()
+if type == "3":
+    type3()
+if type == "4":
+    type4()
 
 screen.mainloop()
 
