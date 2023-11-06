4848ac40ddefde167c342e1cd6e0a635613e8b38
Daklop39 <danko.reya@gmail.com>
2023-11-06 16:35:11 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,38 @@
-print("wk")
-print("wk")
-print("wk")
-print("wk")
-print("wk")
+from tkinter import *
+from turtle import ScrolledCanvas, RawTurtle, TurtleScreen
+import random
+from tkinter import Tk, Text
+root = Tk()
+canvas = ScrolledCanvas(root)
+canvas.pack(side=LEFT)
 
-print("wk")
-print("wk")
-print("wk")
-print("wk")
-print("wk")
+screen = TurtleScreen(canvas)
+turtle = RawTurtle(canvas)
+turtle.hideturtle()
+stx=random.randint(10,100)
+sty=random.randint(10,100)
+
+stxs=0
+stys=0
+turtle.pensize(5)
+turtle.fd(0)
+turtle.pensize(1)
+
+type=input("What type of")    
+
+N=int(input("Diameter of oval"))
+N=N/2
+def type1():                                            #stredova sumernost
+    canvas.create_oval(stx-N,sty-N,stx+N,sty+N)
+    turtle.setposition(100, -110)
+
+if type=="1":
+    type1()
+
+
+
+
+
+
+screen.mainloop()
+
