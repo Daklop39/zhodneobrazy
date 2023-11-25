7df33ae22d8d03326948833eea2c8bdc9dbcceae
Daklop39 <danko.reya@gmail.com>
2023-11-25 17:02:46 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,40 +1,37 @@
-from tkinter import *
-from turtle import ScrolledCanvas, RawTurtle, TurtleScreen
-import random
-from tkinter import Tk, Text
+import turtle
+from turtle import RawTurtle, TurtleScreen
 import tkinter as tk
+from tkinter import *
+import random
 
-root = Tk(className="Sumernosti")
-canvas = ScrolledCanvas(root)
-canvas=tk.Canvas(width=1000,height=1000) 
-canvas.pack(side=LEFT)
+window = tk.Tk()
+window.minsize(width = 720, height = 800)
+canvas = Canvas(window, width = 720, height = 800,bd=0)
+canvas.pack()
 
 screen = TurtleScreen(canvas)
-turtle = RawTurtle(canvas)
-turtle.hideturtle()
+screen.setworldcoordinates(0, 800, 720, 0)
 
-
+#screen.setworldcoordinates(-180, -90, 180, 90)
+#canvas.create_line(0,0,720,800,fill="red")
+#canvas.create_line(720,0,0,800,fill="red")
+quake = RawTurtle(screen)
+#quake.shape("circle")
+#quake.color("red")
+#quake.fd(0)
+#quake.goto(720,800)
 
 type=input("What type of")    
 
-"""def quit():
-    root.destroy()
-
-if type !="1234":
-    quit()"""
-
-
-
-
-
-
 def type1():                                            #stredova sumernost
-    N=3
+    N=50
+    S=3
     stx=random.randint(30,200)
     sty=random.randint(30,200)
-    stxS=random.randint(-100,100)
-    styS=(-100,100)
-    canvas.create_oval(stxS,styS,stxS+N,styS+N,fill="black")
+    stxS=random.randint(100,300)
+    styS=random.randint(100,300)
+    canvas.create_text(stxS+4,styS-10,text="Stred")
+    canvas.create_oval(stxS,styS,stxS+S,styS+S,fill="black")
     canvas.create_oval(stx-N,sty-N,stx+N,sty+N)
     
    
@@ -49,7 +46,7 @@ def type2():                                            #osova sumernost
     canvas.create_text(50,-100,text=("Os sumernosti"))
     canvas.create_text(stx,sty-N-10,text="Vzor")
     canvas.create_oval(stx-N,sty-N,stx+N,sty+N)
-    canvas.create_line(0,-1100,0,1100)
+    canvas.create_line(0,0,0,1100)
     canvas.create_line(stx,sty,stx-(2*stx),sty)
     canvas.create_text((stx-2*stx),sty-N-10,text="Obraz")
     canvas.create_oval((stx-2*stx)-N,sty-N,(stx-2*stx)+N,sty+N)
@@ -84,5 +81,5 @@ if type == "3":
 if type == "4":
     type4()
 
-root.mainloop()
+mainloop()
 
